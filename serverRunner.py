a5c01f8f2b499b3477f6d813686119a4f3fe13e8
axwong <alexwong@tadyen.dev>
2024-01-22 15:54:43 +1100

    server runner script

---
+++ serverRunner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Runs the palworld server into a subprocess
+# Periodically checks the subprocess if it's alive. Restarts the subprocess if it's dead
+
+import subprocess, configparser, os, time
+
+CONFIG_FILE = "config.ini"
+
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+PALSERVER_PATH = config["SERVER_FILES"]["Path"]
+
+PALSERVER_EXEC_FILENAME = "PalServer.sh"
+
+exec_path = os.path.join(
+  PALSERVER_PATH,
+  PALSERVER_EXEC_FILENAME
+)
+exec_path = os.path.normpath(exec_path)
+
+def get_server():
+  return subprocess.Popen("exec", exec_path, shell=True)
+
+server = None
+
+while True:
+  if not server or server.poll():
+    server = get_server()
+  else:
+    time.sleep(10)
